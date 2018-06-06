d41d0c9ef33ca81bb6f65323c2dc68cd4fcb6472
Unknown <irrsfunmaker@gmail.com>
2018-06-06 16:52:21 +0900

    Stage3を仮実装した

--- Prototype0604 量産/Assets/Scripts/Stage1/Stage1Script.cs
+++ Prototype0604 量産/Assets/Scripts/Stage1/Stage1Script.cs
@@ -10,6 +10,7 @@ public class Stage1Script : MonoBehaviour
     //参照
     GameObject FallenTrap;
     GameObject FallenTrap2;
+    GameObject FallenTrap3;
     //パラメータ
     public static string fallenTrapName = "プレス機１";
     public float fallenSpeed = 0.5f;
@@ -24,14 +25,23 @@ public class Stage1Script : MonoBehaviour
 
     public float distanceToTop2 = 5.5f;
     public float distanceToBottom2 = 1;
+
+    public static string fallenTrapName3 = "プレス機3";
+    public float fallenSpeed3 = 0.1f;
+    public float raiseSpeed3 = 0.1f;
+
+    public float distanceToTop3 = 5f;
+    public float distanceToBottom3 = -5;
     // Use this for initialization
     void Start()
     {
         FallenTrap = GameObject.Find("FallenTrapSet");
         FallenTrap2 = GameObject.Find("FallenTrapSet2");
+        FallenTrap3 = GameObject.Find("FallenTrapSet3");
 
         FallenTrap.GetComponent<PressMachineTrapScript>().InitializeParameter(distanceToTop, distanceToBottom, fallenSpeed, raiseSpeed);
         FallenTrap2.GetComponent<PressMachineTrapScript>().InitializeParameter(distanceToTop2, distanceToBottom2, fallenSpeed2, raiseSpeed2);
+        FallenTrap3.GetComponent<PressMachineTrapScript>().InitializeParameter(distanceToTop3, distanceToBottom3, fallenSpeed3, raiseSpeed3);
 
     }
 
@@ -43,6 +53,9 @@ public class Stage1Script : MonoBehaviour
 
         FallenTrap2.GetComponent<PressMachineTrapScript>().
             SetTwoWaysTrap();
+
+        FallenTrap3.GetComponent<PressMachineTrapScript>().
+           SetTwoWaysTrap();
 
         if (UIScript.parameter_ChangeEnable)
         {
@@ -59,6 +72,7 @@ public class Stage1Script : MonoBehaviour
 
         FallenTrap.GetComponent<PressMachineTrapScript>().InitializeParameter(distanceToTop, distanceToBottom, fallenSpeed, raiseSpeed);
         FallenTrap2.GetComponent<PressMachineTrapScript>().InitializeParameter(distanceToTop2, distanceToBottom2, fallenSpeed2, raiseSpeed2);
+        FallenTrap3.GetComponent<PressMachineTrapScript>().InitializeParameter(distanceToTop3, distanceToBottom3, fallenSpeed3, raiseSpeed3);
 
     }
 }
